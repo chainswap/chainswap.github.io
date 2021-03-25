435abd58a8b7242cef75c556d4394f86e87f49f6
TengYingWei <734970600@qq.com>
2021-03-26 00:39:14 +0800

    improve favicon && links

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,5 +1,5 @@
 export const MEDIUM_PATH = "https://medium.com/@chain_swap";
-export const GITHUB_PATH = "https://github.com/chainswap/contracts";
+export const GITHUB_PATH = "https://github.com/chainswap";
 export const TELEGRAM_PATH = "https://t.me/chainswapann";
 export const TWITTER_PATH = "https://twitter.com/chain_swap";
 
--- src/modules/launch-pop-up/LaunchPopUp.tsx
+++ src/modules/launch-pop-up/LaunchPopUp.tsx
@@ -7,7 +7,7 @@ import styles from "./LaunchPopUp.module.scss";
 const LINKS = [
 	{
 		caption: "Cross chain assets",
-		href: "#",
+		href: "https://app.chainswap.exchange",
 	},
 	{
 		caption: (
