ef1ab1bf11828d64e7c3a5ac0ec9149b037673a1
antimatter-finance <78986458+antimatter-finance@users.noreply.github.com>
2021-04-07 00:08:31 +0800

    Update const.ts

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,6 +1,6 @@
 export const MEDIUM_PATH = "https://medium.com/@chain_swap";
 export const GITHUB_PATH = "https://github.com/chainswap";
-export const TELEGRAM_PATH = "https://t.me/chainswapann";
+export const TELEGRAM_PATH = "https://t.me/chainswap";
 export const TWITTER_PATH = "https://twitter.com/chain_swap";
 
 export const WHITEPAPER = "https://github.com/chainswap/chainswap.github.io";
