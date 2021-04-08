abd7fe0137a51def7d2283abbceddc57551d5d4e
yuhuakukude <15577334054@163.com>
2021-04-08 14:37:44 +0800

    Improve description

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Home } from "../src/pages/home";
 
 const Index = pageWithLayout(
 	() => <Home />,
-	() => <Layout title="Chain Swap" description="The hub for all smart chains" />
+	() => <Layout title="Chain Swap" description="Crosschain asset, one click crosschain deployment." />
 );
 
 export default Index;
